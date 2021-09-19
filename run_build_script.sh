2d9c3970e177d4d4c6b1207b1b55f816326670aa
adrianutec2 <60326319+adrianutec2@users.noreply.github.com>
2021-09-18 23:10:46 -0300

    Update run_build_script.sh

--- run_build_script.sh
+++ run_build_script.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "Run build script for demo pipeline"
 docker build -t webserver .
-docker run -it --rm -d -p 8181:80 --name web webserver
+docker run -d -p 8181:80 --name web webserver
